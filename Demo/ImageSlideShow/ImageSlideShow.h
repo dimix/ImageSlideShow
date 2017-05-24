2e140b24aa501f2ffa04c42d65af6a0870ba9270
Lasse Thorup Hansen <lha@trifork.com>
2017-05-24 14:02:26 +0200

    Added the possibilty to make a framework to use for carthage. Run ‘carthage build --no-skip-current’ inside the /Demo folder to validate

---
+++ Demo/ImageSlideShow/ImageSlideShow.h
@@ -0,0 +1,19 @@
+//
+//  ImageSlideShow.h
+//  ImageSlideShow
+//
+//  Created by Lasse Hansen on 24/05/2017.
+//  Copyright © 2017 Dimitri Giani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageSlideShow.
+FOUNDATION_EXPORT double ImageSlideShowVersionNumber;
+
+//! Project version string for ImageSlideShow.
+FOUNDATION_EXPORT const unsigned char ImageSlideShowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageSlideShow/PublicHeader.h>
+
+
